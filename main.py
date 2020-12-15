fe8502de9e6d655c10da3fee9874f0fdd220e8a1
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-15 12:10:44 -0600

    feat(core): Add basic scraper

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+import random
 
+
+class Scraper:
+    def openURL(self):
+        with open("brain/urls.txt") as f:
+            # Update PATH with your local Chrome WebDriver's:
+            PATH = "/Users/filippofonseca/Developer/SDKs/chromedriver"
+            lines = f.readlines()
+            driver = webdriver.Chrome(PATH)
+            driver.get(random.choice(lines))
+            print(driver.title)
+
+
+scraper = Scraper()
+
+scraper.openURL()
